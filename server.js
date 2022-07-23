6226ff1072ee0009d6583df1f4f27c95eef2f424
= <=>
2022-07-23 15:14:41 -0300

    \n removed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/:name', (req,res)=>{
 });
 
 app.get('/', (req,res)=>{
-    res.send({success:true, message:"Oi, meu amor! \nQuero que saiba que você é e está se tornando muito especial na minha vida. \n\nDesculpa eu não ser o melhor namorado, mas eu prometo que vou tentar ser. Graças a você dei um grande passo de pesquisa pro meu TCC. \n\nIsso é uma maquina virtual que pode ser acessado por quanquer canto do mundo e que será a rota da API da minha aplicação pro TCC. Um beijo do seu namoradinho que te ama mauito! \n\n\n(vou tentar ser menos estressado, prometo)"});
+    res.send({success:true, message:"Oi, meu amor! Quero  dsdfsdf  que saiba que você é e está se tornando muito especial na minha vida. Desculpa eu não ser o melhor namorado, mas eu prometo que vou tentar ser. Graças a você dei um grande passo de pesquisa pro meu TCC. Isso é uma maquina virtual que pode ser acessado por quanquer canto do mundo e que será a rota da API da minha aplicação pro TCC. Um beijo do seu namoradinho que te ama mauito! (vou tentar ser menos estressado, prometo)"});
 });
 
 const port = 7000;
