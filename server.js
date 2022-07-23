003ae6e7260d863dbca444855f7a327f4476391c
= <=>
2022-07-23 15:29:36 -0300

    app header config

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 
 const app = express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+ })
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
